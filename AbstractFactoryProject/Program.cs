8028c887841b3a95d364c9e90579e6b9273f38a8
rakdav <rakdav@inbox.ru>
2025-09-26 11:20:25 +0200

    AbsMethod

---
+++ AbstractFactoryProject/Program.cs
@@ -0,0 +1,47 @@
+﻿interface IEngine
+{
+    void ReleaseEngine();
+}
+class JapaneseEngine : IEngine
+{
+    public void ReleaseEngine() => Console.WriteLine("japanese engine");
+}
+class RussianEngine : IEngine
+{
+    public void ReleaseEngine() => Console.WriteLine("russian engine");
+}
+interface ICar
+{
+    void ReleaseCar(IEngine engine);
+}
+class JapaneseCar : ICar
+{
+    public void ReleaseCar(IEngine engine)
+    {
+        Console.WriteLine("assembled a Japanese car");
+        engine.ReleaseEngine();
+    }
+}
+class RussianCar : ICar
+{
+    public void ReleaseCar(IEngine engine)
+    {
+        Console.WriteLine("assembled a Russian car");
+        engine.ReleaseEngine();
+    }
+}
+interface IFactory
+{
+    IEngine CreateEngine();
+    ICar CreateCar();
+}
+class JapaneseFactory : IFactory
+{
+    public ICar CreateCar()=>new JapaneseCar();
+    public IEngine CreateEngine()=>new JapaneseEngine();
+}
+class RussianFactory : IFactory
+{
+    public ICar CreateCar() => new RussianCar();
+    public IEngine CreateEngine() => new RussianEngine();
+}
---
+++ BuilderProject/Program.cs
@@ -0,0 +1,60 @@
+﻿IDeveloper androidDeveloper = new AndroidDeveloper();
+Director director=new Director(androidDeveloper);
+Phone samsung=director.MountFullPhone();
+Console.WriteLine(samsung.AboutPhone());
+IDeveloper iphoneDeveloper=new IphoneDeveloper();
+director.SetDeveloper(iphoneDeveloper);
+Phone iphone=director.MountOnlyPhone();
+Console.WriteLine(iphone.AboutPhone());
+
+class Phone
+{
+    private string data;
+    public Phone() => data = "";
+    public string AboutPhone() => data;
+    public void AppendData(string str) => data += str;
+}
+interface IDeveloper
+{
+    void CreateDisplay();
+    void CreateBox();
+    void SystemInstall();
+    Phone GetPhone();
+}
+class AndroidDeveloper:IDeveloper
+{
+    private Phone phone;
+    public AndroidDeveloper()=>phone = new Phone();
+    public void CreateBox()=>phone.AppendData("(create) corpus Samsung");
+    public void CreateDisplay()=> phone.AppendData("(create) display Samsung");
+    public Phone GetPhone() => phone;
+    public void SystemInstall() { phone.AppendData("(install) system Android"); }
+}
+class IphoneDeveloper : IDeveloper
+{
+    private Phone phone;
+    public IphoneDeveloper()=>phone=new Phone();
+    public void CreateBox() => phone.AppendData("(create) corpus Iphone");
+    public void CreateDisplay() => phone.AppendData("(create) display Iphone");
+    public Phone GetPhone() => phone;
+    public void SystemInstall() { phone.AppendData("(install) system IOS"); }
+}
+class Director
+{
+    private IDeveloper developer;
+    public Director(IDeveloper _developer) => developer = _developer;
+    public void SetDeveloper(IDeveloper _developer)=> developer = _developer;
+    public Phone MountOnlyPhone()
+    {
+        developer.CreateBox();
+        developer.CreateDisplay();
+        return developer.GetPhone();
+    }
+    public Phone MountFullPhone()
+    {
+        developer.CreateBox();
+        developer.CreateDisplay();
+        developer.SystemInstall();
+        return developer.GetPhone();
+    }
+}
