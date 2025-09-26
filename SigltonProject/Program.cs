e0ca210dd8bbb1b1fba7442b586d67fa503c716a
rakdav <rakdav@inbox.ru>
2025-09-26 09:41:22 +0200

    Добавьте файлы проекта.

---
+++ SigltonProject/Program.cs
@@ -0,0 +1,22 @@
+﻿DatabaseHelper.GetConnection().InsertData("123");
+Console.WriteLine("Data: "+DatabaseHelper.GetConnection().SelectData());
+public class DatabaseHelper
+{
+    private string _data;
+    private static DatabaseHelper Instance;
+    private DatabaseHelper() => Console.WriteLine("connection to database");
+    public static DatabaseHelper GetConnection()
+    {
+        if(Instance == null)
+            Instance = new DatabaseHelper();
+        return Instance;
+    }
+    public string SelectData()=>_data;
+    public void InsertData(string d)
+    {
+        Console.WriteLine("new data: " + d + "(entered into the database)");
+        _data = d;
+    }
+}
+
+
