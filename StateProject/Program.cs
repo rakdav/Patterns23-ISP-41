7adc3bfe396400ba76e7c21c894bb53deaf5834b
rakdav <rakdav@inbox.ru>
2025-11-13 14:43:11 +0200

    State

---
+++ StateProject/Program.cs
@@ -0,0 +1,74 @@
+﻿TrafficLight traphicLight = new TrafficLight(new GreenState());
+traphicLight.NextState();
+traphicLight.NextState();
+traphicLight.NextState();
+traphicLight.PreviousState();
+traphicLight.PreviousState();
+traphicLight.PreviousState();
+abstract class State
+{
+    protected TrafficLight trafficLight;
+    public TrafficLight TrafficLight { set => trafficLight = value; }
+    public abstract void NextState();
+    public abstract void PreviousState();
+}
+
+class TrafficLight
+{
+    private State state;
+
+    public TrafficLight(State state) => SetState(state);
+    public void SetState(State st)
+    {
+        this.state = st;
+        this.state.TrafficLight=this;
+    }
+    public void NextState()
+    {
+        state.NextState();
+    }
+    public void PreviousState()
+    {
+        state.PreviousState();
+    }
+}
+class GreenState : State
+{
+    public override void NextState()
+    {
+        Console.WriteLine("from green to yellow");
+        trafficLight.SetState(new YellowState());
+    }
+    public override void PreviousState()
+    {
+        Console.WriteLine("Green color");
+    }
+}
+class YellowState : State
+{
+    public override void NextState()
+    {
+        Console.WriteLine("from yellow to red");
+        trafficLight.SetState(new RedState());
+    }
+
+    public override void PreviousState()
+    {
+        Console.WriteLine("from yellow to green");
+        trafficLight.SetState(new GreenState());
+    }
+}
+
+class RedState : State
+{
+    public override void NextState()
+    {
+        Console.WriteLine("red color");
+    }
+
+    public override void PreviousState()
+    {
+        Console.WriteLine("from red to yellow");
+        trafficLight.SetState(new YellowState());
+    }
+}
---
+++ VisitorProject/Program.cs
@@ -0,0 +1,42 @@
+﻿List<IPlace> places = new List<IPlace> { new Zoo(),
+new Cinema(),new Circus()};
+foreach(IPlace place in places)
+{
+    HolidayMaker visitor = new HolidayMaker();
+    place.Accept(visitor);
+    Console.WriteLine(visitor.Value);
+}
+interface IPlace
+{
+    void Accept(IVisitor visitor);
+}
+class Zoo : IPlace
+{
+    public void Accept(IVisitor v) => v.Visit(this);
+}
+class Cinema : IPlace
+{
+    public void Accept(IVisitor v) => v.Visit(this);
+}
+class Circus : IPlace
+{
+    public void Accept(IVisitor v) => v.Visit(this);
+}
+interface IVisitor
+{
+    void Visit(Zoo zoo);
+    void Visit(Cinema cinema);
+    void Visit(Circus circus);
+}
+class HolidayMaker : IVisitor
+{
+    public string Value;
+
+    public void Visit(Zoo zoo) => Value = "zoo";
+
+    public void Visit(Cinema cinema)=> Value = "cinema";
+
+    public void Visit(Circus circus)=> Value = "circus";
+
+}
+
